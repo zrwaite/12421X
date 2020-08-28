c13de105b9dda1b0fa41c966d734df72f63adbb7
zrwaite <68486874+zrwaite@users.noreply.github.com>
2020-08-28 14:48:56 -0400

    Added temperature prints to screen

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@
 #include "vex.h"
 #include "Motor.h"
 #include <cmath>
+#include <string>
 
 using namespace vex;
 
@@ -40,69 +41,26 @@ Motor Input2("Input2");
 Motor Roller("Roller");
 Motor Output("Output");
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
-
-void pre_auton(void) {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
-}
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
-void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
-}
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
-void usercontrol(void) {
-  
-    // Sleep the task for a short amount of time to prevent wasted resources.
-    vex::task::sleep(20);
-    while(1){
-      Left1.drive(Controller1.Axis3.position());
-      Left2.drive(Controller1.Axis3.position());
-      Right1.drive(Controller1.Axis2.position());
-      Right2.drive(Controller1.Axis2.position());
-    
-
-    wait(20, msec); // Sleep the task for a short amount of time to prevent wasted resources.
-    }
-}
+double temps[8];
+double tempIndex = 1;
 
 //Shorter wait function in milliseconds. 
 void wait(int time){
   vex::task::sleep(time);
 }
+
+void clear(){
+  Controller1.Screen.clearScreen();
+}
+
+template <class T>
+void p(T words){
+  Controller1.Screen.print(words);
+}
+
+void newLine(){
+  Controller1.Screen.newLine();
+} 
 
 //Function takes Speed,  number of motors, and the names of the motors to be turned on. 
 void drives(int speed, int num, ...){
@@ -158,6 +116,126 @@ void stops(int num, ...){
   va_end(args);
 }
 
+void temp(){
+  //Error Reporting
+  temps[0] = MLeft1.temperature(percent);
+  temps[1] = MLeft2.temperature(percent);
+  temps[2] = MRight1.temperature(percent);
+  temps[3] = MRight2.temperature(percent);
+  temps[4] = MInput1.temperature(percent);
+  temps[5] = MInput2.temperature(percent);
+  temps[6] = MOutput.temperature(percent);
+  temps[7] = MRoller.temperature(percent);
+
+  clear();
+  if (tempIndex >= 9){
+    tempIndex = 1;
+  }
+  switch (int(floor(tempIndex))){
+    case 1:
+      p("Left1: "); p(temps[0]); newLine();
+      p("Left2: "); p(temps[1]); newLine();
+      p("Right1: "); p(temps[2]); newLine();
+      break;
+    case 2:
+      p("Left2: "); p(temps[1]); newLine();
+      p("Right1: "); p(temps[2]); newLine();
+      p("Right2: "); p(temps[3]); newLine();
+      break;
+    case 3:
+      p("Right1: "); p(temps[2]); newLine();
+      p("Right2: "); p(temps[3]); newLine();
+      p("Input1: "); p(temps[4]); newLine();
+      break;
+    case 4:
+      p("Right2: "); p(temps[3]); newLine();
+      p("Input1: "); p(temps[4]); newLine();
+      p("Input2: "); p(temps[5]); newLine();
+      break;
+    case 5:
+      p("Input1: "); p(temps[4]); newLine();
+      p("Input2: "); p(temps[5]); newLine();
+      p("Output: "); p(temps[6]); newLine();
+      break;
+    case 6:
+      p("Input2: "); p(temps[5]); newLine();
+      p("Output: "); p(temps[6]); newLine();
+      p("Roller: "); p(temps[7]); newLine();
+      break;
+    case 7:
+      p("Output: "); p(temps[6]); newLine();
+      p("Roller: "); p(temps[7]); newLine();
+      p("Left1: "); p(temps[0]); newLine();
+      break;
+    case 8:
+      p("Roller: "); p(temps[7]); newLine();
+      p("Left1: "); p(temps[0]); newLine();
+      p("Left2: "); p(temps[1]); newLine();
+      break;
+    default:
+      p("Error");
+  }
+  tempIndex+=0.02;
+}
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  
+    // Sleep the task for a short amount of time to prevent wasted resources.
+    vex::task::sleep(20);
+    while(1){
+      Left1.drive(Controller1.Axis3.position());
+      Left2.drive(Controller1.Axis3.position());
+      Right1.drive(Controller1.Axis2.position());
+      Right2.drive(Controller1.Axis2.position());
+      temp(); //Takes motor temperatures and prints to controller screen
+      
+    }
+}
+
 //
 // Main will set up the competition functions and callbacks.
 //
@@ -174,6 +252,8 @@ int main() {
 
 
 void example(){
+
+  // Written in plain VexCode:
   /*
   LeftDrive1.spin(vex::directionType::fwd,  200, vex::velocityUnits::rpm);
   vex::task::sleep(500);
@@ -181,7 +261,6 @@ void example(){
   vex::task::sleep(50);
   LeftDrive1.stop(vex::brakeType::coast);
   vex::task::sleep(50);
-
   LeftDrive1.spin(vex::directionType::fwd,  200, vex::velocityUnits::rpm);
   LeftDrive2.spin(vex::directionType::fwd,  200, vex::velocityUnits::rpm);
   RightDrive1.spin(vex::directionType::fwd,  200, vex::velocityUnits::rpm);
@@ -197,10 +276,11 @@ void example(){
   RightDrive1.stop(vex::brakeType::coast);
   RightDrive2.stop(vex::brakeType::coast);
   */
+
+  //Written in our code:
   Left1.drive(200);
   wait(500);
   Left1.stop();
-
   drives(200, 4, "Left1", "Left2","Right1", "Right2");
   wait(500);
   stops(4, "Left1", "Left2","Right1", "Right2");
