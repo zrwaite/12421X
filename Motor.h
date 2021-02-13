1ebcc1dfc597b1745a92a7e2ef82e6c0ca74b4fd
JJDoleweerd <32210266+JJDoleweerd@users.noreply.github.com>
2021-02-13 08:25:26 -0500

    Add files via upload

--- Motor.h
+++ Motor.h
@@ -1,20 +1,20 @@
 using namespace vex;
 using namespace std;
-
+ 
 #ifndef MOTOR_H
 #define MOTOR_H
 #include <string>
-
+ 
 class Motor{
-  public:
-    Motor(string name); //Constructor
-    string getName(); //Getter for name
-    void wait(int time); //Shorter wait function
-    void go(int speed); //Shorter drive function
-    void brake1(); //Shorter brake function (brake)
-    void brake2(); //Shorter brake function (coast)
-    void stop(); //Brake 1 and 2 with wait
-  private:
-    string name;
+ public:
+   Motor(string name); //Constructor
+   string getName(); //Getter for name
+   void wait(int time); //Shorter wait function
+   void go(int speed); //Shorter drive function
+   void brake1(); //Shorter brake function (brake)
+   void brake2(); //Shorter brake function (coast)
+   void stop(); //Brake 1 and 2 with wait
+ private:
+   string name;
 };
 #endif // MOTOR_H
--- robot-config.cpp
+++ robot-config.cpp
@@ -8,13 +8,13 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
-motor MLeft1 = motor(PORT11, ratio18_1, false);
-motor MLeft2 = motor(PORT8, ratio18_1, false);
+motor MLeft1 = motor(PORT12, ratio18_1, false);
+motor MLeft2 = motor(PORT7, ratio18_1, false);
 motor MRight1 = motor(PORT9, ratio18_1, false);
 motor MRight2 = motor(PORT5, ratio18_1, false);
-motor MRoller = motor(PORT15, ratio18_1, false);
-motor MInput1 = motor(PORT18, ratio18_1, false);
-motor MInput2 = motor(PORT7, ratio18_1, false);
+motor MRoller = motor(PORT13, ratio18_1, false);
+motor MInput1 = motor(PORT11, ratio18_1, false);
+motor MInput2 = motor(PORT14, ratio18_1, false);
 motor MOutput = motor(PORT10, ratio18_1, false);
 controller Controller1 = controller(primary);
 
